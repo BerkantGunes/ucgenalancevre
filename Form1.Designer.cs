ff0054f3f44d819c1eb7026f8eddc1c9d73dbebd
Berkant Gunes <gunesberkant96@gmail.com>
2025-02-17 20:02:32 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ucgenalancevre
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKenar3 = new System.Windows.Forms.TextBox();
+            this.txtKenar2 = new System.Windows.Forms.TextBox();
+            this.txtTaban = new System.Windows.Forms.TextBox();
+            this.txtYukseklik = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "alan hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "taban";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "yukseklik";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(262, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "kenar2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(262, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "kenar3";
+            // 
+            // txtKenar3
+            // 
+            this.txtKenar3.Location = new System.Drawing.Point(331, 205);
+            this.txtKenar3.Name = "txtKenar3";
+            this.txtKenar3.Size = new System.Drawing.Size(100, 22);
+            this.txtKenar3.TabIndex = 5;
+            // 
+            // txtKenar2
+            // 
+            this.txtKenar2.Location = new System.Drawing.Point(331, 141);
+            this.txtKenar2.Name = "txtKenar2";
+            this.txtKenar2.Size = new System.Drawing.Size(100, 22);
+            this.txtKenar2.TabIndex = 6;
+            // 
+            // txtTaban
+            // 
+            this.txtTaban.Location = new System.Drawing.Point(118, 141);
+            this.txtTaban.Name = "txtTaban";
+            this.txtTaban.Size = new System.Drawing.Size(100, 22);
+            this.txtTaban.TabIndex = 7;
+            // 
+            // txtYukseklik
+            // 
+            this.txtYukseklik.Location = new System.Drawing.Point(118, 205);
+            this.txtYukseklik.Name = "txtYukseklik";
+            this.txtYukseklik.Size = new System.Drawing.Size(100, 22);
+            this.txtYukseklik.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(289, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "cevre hesapla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtYukseklik);
+            this.Controls.Add(this.txtTaban);
+            this.Controls.Add(this.txtKenar2);
+            this.Controls.Add(this.txtKenar3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKenar3;
+        private System.Windows.Forms.TextBox txtKenar2;
+        private System.Windows.Forms.TextBox txtTaban;
+        private System.Windows.Forms.TextBox txtYukseklik;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ucgenalancevre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int taban, yukseklik, alan;
+
+            taban = Convert.ToInt32(txtTaban.Text);
+            yukseklik = Convert.ToInt32(txtYukseklik.Text);
+
+            alan = (taban * yukseklik) / 2;
+
+            MessageBox.Show("Ucgenin alani: " + alan);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int taban, kenar2, kenar3, cevre;
+
+            taban = Convert.ToInt32(txtTaban.Text);
+            kenar2 = Convert.ToInt32(txtKenar2.Text);
+            kenar3 = Convert.ToInt32(txtKenar3.Text);
+
+            cevre = taban + kenar2 + kenar3;
+
+            MessageBox.Show("Ucgenin cevresi: " +  cevre);
+        }
+    }
+}
